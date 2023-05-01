427483be4d070220c0b60f97f672a420413606f6
Swen Gross <swen_g@t-online.de>
2023-05-01 19:11:44 +0200

    automations (#6)

--- setup.py
+++ setup.py
@@ -1,20 +1,65 @@
-from setuptools import setup, find_packages
+import os
+
+from setuptools import setup
 
 PLUGIN_ENTRY_POINT = 'ovos-tts-plugin-marytts = ovos_tts_plugin_marytts:MaryTTS'
 SAMPLE_CONFIGS = 'ovos-tts-plugin-marytts.config = ' \
                  'ovos_tts_plugin_marytts:MaryTTSPluginConfig'
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_tts_plugin_marytts', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+    
 setup(
     name='ovos-tts-plugin-marytts',
-    version="0.0.1",
+    version=get_version(),
     description='A marytts plugin for OpenVoiceOS',
     url='https://github.com/OpenVoiceOS/ovos-tts-plugin-marytts',
     author='jarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache2',
-    packages=find_packages(),
-    install_requires=["ovos-plugin-manager~=0.0"],
+    packages=['ovos_tts_plugin_marytts'],
+    install_requires=required("requirements/requirements.txt"),
+    package_data={'': package_files('ovos_tts_plugin_marytts')},
     zip_safe=True,
     classifiers=[
         'Intended Audience :: Developers',
